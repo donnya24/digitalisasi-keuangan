aad7cb82f83b8d02f164fe9e6d8243c34662433a
donnya24 <donny.23024@mhs.unesa.ac.id>
2026-03-02 08:52:33 +0700

    delete component tidak diperlukan

--- resources/views/components/input-label.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['value', 'required' => false])
-
-<label {{ $attributes->merge(['class' => 'input-label']) }}>
-    {{ $value ?? $slot }}
-    @if($required)
-        <span class="text-red-500 ml-1">*</span>
-    @endif
-</label>
